39693706a7e6d1a9b65c9467483f1cc93ea34960
Isabelle <isabelle.annie.karlsson@gmail.com>
2022-09-13 10:07:42 +0200

    Feature 008: Started with consultants page

--- src/App.js
+++ src/App.js
@@ -1,30 +1,41 @@
-import { useState, useEffect } from "react"
+// import { useState, useEffect } from "react"
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Consultants from "./components/Consultants";
+import Home from "./components/Home";
+
+// const baseLink = process.env.REACT_APP_API_ADDRESS || "http://localhost:3001"
 
 function App() {
 
-  const [userName, setUserName] = useState('')
+  // const [userName, setUserName] = useState('')
 
-  const getName = async () => {
-    const response = await fetch(`${process.env.REACT_APP_API_ADDRESS}/api/v0/user-name`);
-    const user = await response.json();
-    console.log(user)
-    setUserName(user.name)
+  // const getName = async () => {
+  //   const response = await fetch(`${baseLink}/api/v0/user-name`);
+  //   const user = await response.json();
+  //   console.log(user)
+  //   setUserName(user.name)
 
-  }
-  useEffect(() => {
-    getName()
-  }, [])
+  // }
+  // useEffect(() => {
+  //   getName()
+  // }, [])
 
 
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <h2>Hello { userName }!</h2>
-        <h1>Welcome to One Source.</h1>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/consultants" element={<Consultants />} />
+      </Routes>
+    </Router>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <h2>Hello {userName}!</h2>
+    //     <h1>Welcome to One Source.</h1>
+    //   </header>
+    // </div>
   );
 }
 

547d222ad6ef03c802daecabfd312db98b7ec05a
Vitoleander <89918725+Vitoleander@users.noreply.github.com>
2022-09-16 10:07:14 +0200

    chatbot changes

--- src/components/Chat/Chatbot.js
+++ src/components/Chat/Chatbot.js
@@ -1,9 +1,10 @@
 import Chatbot from 'react-simple-chatbot';
+import { ThemeProvider } from 'styled-components';
 
 const steps = [
     {
         id: '0',
-        message: 'Puggy to the rescue',
+        message: 'Puggy to the rescue!',
         trigger: '1',
     }, {
         id: '1',
@@ -27,11 +28,29 @@ const steps = [
     }
 ];
 
+const theme = {
+    background: '#ffffff',
+    headerBgColor: '#005794',
+    headerFontSize: '20px',
+    botBubbleColor: '#005794',
+    headerFontColor: 'white',
+    botFontColor: 'white',
+    userBubbleColor: '#005794',
+    userFontColor: 'white',
+};
+
+const config = {
+    botAvatar: 'https://images.unsplash.com/photo-1523626797181-8c5ae80d40c2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=435&q=80',
+    floating: true,
+};
+
 function Chat () {
     return (
         <div className="Chatbot">
-            <h2>Welcome to Puggy chat!!</h2>
-            <Chatbot steps={steps} />
+            <ThemeProvider theme={theme}>
+                <Chatbot steps={steps} />
+            </ThemeProvider>
+            
         </div>
     );
 }

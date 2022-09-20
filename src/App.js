c02d814a4623b1f44a45e571e78971400092e4aa
Vitoleander <89918725+Vitoleander@users.noreply.github.com>
2022-09-20 18:54:50 +0200

    deleting chatbot for a fresh start

--- src/App.js
+++ src/App.js
@@ -1,16 +1,6 @@
+import React from 'react';
 import { useState } from 'react';
 import './App.css';
-<<<<<<< HEAD
-<<<<<<< HEAD
-import {  BrowserRouter as Router,} from "react-router-dom";
-import Header from "./components/Header";
-import Home from "./components/Home";
-import LoginContainer from "./components/LoginContainer";
-import Consultants from "./components/Consultants";
-import PugEngine from "./Chatbot/PugEngine";
-=======
-=======
->>>>>>> dadd638d20793bdc53a851ea7c08753a7dd57fc8
 import {
   BrowserRouter as Router,
   Switch,
@@ -21,11 +11,9 @@ import Header from './components/Header';
 import Home from './components/Home';
 import LoginContainer from './components/LoginContainer';
 import Consultants from './components/Consultants';
-import Chat from './components/Chat/Chatbot';
-<<<<<<< HEAD
->>>>>>> 6fc0ef375da8c70a37e956f8018cad706b1d01ef
-=======
->>>>>>> dadd638d20793bdc53a851ea7c08753a7dd57fc8
+
+
+
 
 function App() {
   const [toggleHide, setToggleHide] = useState('hidden');
@@ -43,7 +31,6 @@ function App() {
         <LoginContainer toggleHide={toggleHide} setToggleHide={setToggleHide} handleClick={handleClick} setUser={setUser} />
         <Header handleClick={handleClick} user={user} setUser={setUser} />
         <Home />
-        <div className="Chat"><Chat /></div>
         <Consultants />
       </Router>
     </div>

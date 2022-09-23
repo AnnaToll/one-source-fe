eecd1288bacc0a43a536d6642d05f0613fb73e33
Pernilla Lindström <pernilla_lindstrom@outlook.com>
2022-09-23 11:58:04 +0200

    Feature 006: fixed some css in contact page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -11,20 +11,21 @@ function Contact() {
     e.preventDefault();
   };
   return (
-    <section className="contact-container">
-        
+    <section className='contact-container'>
+      
         <h1>Contact</h1>
 
-        
+        <div className='adress'>
         <p>Adress:</p>
         <p>One Sourcevägen 1</p>
         <p>111 11 Stockholm</p>
         <p>Phone: +4611-111 111</p>
+        </div>
         
-        <form onSubmit={handleSubmit}>
-          <input type='text' onChange={ (e) => setName(e.target.value)} value={name}/>
-          <input type='email'onChange={ (e) => setEmail(e.target.value)} value={email}/>
-          <input type='text' onChange={ (e) => setMessage(e.target.value)} value={message}/>
+        <form className='contactForm' onSubmit={handleSubmit}>
+          <input placeholder='Name' type='text' onChange={ (e) => setName(e.target.value)} value={name}/>
+          <input placeholder='Email' type='email'onChange={ (e) => setEmail(e.target.value)} value={email}/>
+          <input placeholder='Enter message here' size="50" type='text' onChange={ (e) => setMessage(e.target.value)} value={message}/>
           <button>Send</button>
         </form>
 

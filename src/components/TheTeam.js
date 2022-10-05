3df1287f370a67ea3f68aab3502c3565fdcf24a4
Isabelle <isabelle.annie.karlsson@gmail.com>
2022-10-05 15:32:24 +0200

    Feature 008: moved css code for team component

--- src/components/TheTeam.js
+++ src/components/TheTeam.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
-// import './components.css';
+import './components.css';
 // import styles from './Consultants.module.css';
-import styles from './TheTeam.module.css';
+// import styles from './TheTeam.module.css';
 
 const baseLink = process.env.REACT_APP_API_ADDRESS || 'http://localhost:3001';
 
@@ -21,25 +21,21 @@ function TheTeam() {
   }, []);
 
   return (
-    <main className={styles.mainContainer}>
-      <div className={styles.headingContainer}>
-        {/* <h2 className={styles.heading2}>The <br /><span className={styles.dreamteam} >One Source</span> <br /> Team!</h2> */}
-        <h2 className={styles.heading2}>We wouldn&apos;t be One Source without our <span className={styles.dreamteam}>awesome dreamteam</span>!</h2>
-        <img src="/IMG/—Pngtree—arc arrow vector diagram_5054191_2.png" alt="arrow" className={styles.arrow} />
-        {/* <div><p><i className="arrow right"></i></p></div> */}
+    <main className='teamContainer'>
+      <div className='headingContainer'>
+        {/* <h2 className="headingTeam">The <br /><span className="dreamteam" >One Source</span> <br /> Team!</h2> */}
+        <h2 className='headingTeam'>We wouldn&apos;t be One Source without our <span className='dreamteam'>awesome dreamteam</span>!</h2>
+        <img src="/IMG/—Pngtree—arc arrow vector diagram_5054191_2.png" alt="arrow" className='arrow' />
       </div>
-      <section className={styles.teamMemberContainer}>
+      <section className="teamMemberContainer">
         {consultants.map((person) => (
-          <div className={styles.teamMember} key={person.id}>
-            <img src={`/IMG/${person.image}`} alt={person.name} className={styles.image} />
+          <div className="teamMember" key={person.id}>
+            <img src={`/IMG/${person.image}`} alt={person.name} className="teamImage" />
             <h3>{person.name}</h3>
-            {/* <div><span className={styles.icons}><i className="bi bi-phone-fill"></i></span><span>{person.phone}</span></div> */}
-            {/* <div><span className={styles.icons}><i className="bi bi-envelope-fill"></i></span><span>{person.email}</span></div> */}
-            <p className={styles.skills}>frontend developer</p>
-            <div className={styles.separator}></div>
-            {/* <hr className={styles.separator} /> */}
-            <p className={styles.aboutTeamMember}>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorem quidem eius labore incidunt voluptate!</p>
-            <div className={styles.socialIconsContainer}>
+            <p className="skills">frontend developer</p>
+            <div className="separator"></div>
+            <p className="aboutTeamMember">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorem quidem eius labore incidunt voluptate!</p>
+            <div className="socialIconsContainer">
               <i className="bi bi-linkedin"></i>
               <i className="bi bi-github"></i>
             </div>
